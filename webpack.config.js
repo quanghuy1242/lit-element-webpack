1d12ece5d0b28fb8a1eb293ed82ec1829bb33dfe
quanghuy1242 <katygaga124@gmail.com>
2019-09-15 12:23:45 +0700

    copy styles.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
+const merge = require('webpack-merge');
 const path = require('path');
-const { createDefaultConfig } = require('@open-wc/building-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+// // For IE Support
 // const { createCompatibilityConfig } = require('@open-wc/building-webpack');
-// module.exports = createCompatibilityConfig({
+// const configs = createCompatibilityConfig({
 //   input: path.resolve(__dirname, './index.html'),
 // });
 
-module.exports = createDefaultConfig({
+// module.exports = [
+//   merge(configs[0], {
+//     plugins: [
+//       new CopyWebpackPlugin([
+//         'styles.css',
+//       ]),
+//     ],
+//   }),
+//   configs[1],
+// ];
+
+// For modern browsers
+const { createDefaultConfig } = require('@open-wc/building-webpack');
+const config = createDefaultConfig({
   input: path.resolve(__dirname, './index.html'),
 });
+
+module.exports = merge(config, {
+  plugins: [
+    new CopyWebpackPlugin([
+      'styles.css',
+    ]),
+  ],
+})
